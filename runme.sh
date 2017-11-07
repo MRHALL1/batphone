2a427d646cd0115ec290e23c20948042fe301c2f
MRHALL1 <mrhall1@mail.lipscomb.edu>
2017-11-07 18:08:54 -0500

    Project uploaded

---
+++ runme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+source sourceme.sh
+
+# ./gradlew build --debug >& 103117.gradlew.build.out.test
+
+
+
+./gradlew assembleDebug  --debug >& 103117.gradlew.assembleDebugassembleDebug.out.test
+
+
+####   bash -x ./runme.sh 
+
+# gradlew clean
+
---
+++ test.c
@@ -0,0 +1,19 @@
+ /* confdefs.h */
+ #define PACKAGE_NAME "libsodium"
+ #define PACKAGE_TARNAME "libsodium"
+ #define PACKAGE_VERSION "1.0.12"
+ #define PACKAGE_STRING "libsodium 1.0.12"
+ #define PACKAGE_BUGREPORT "https://github.com/jedisct1/libsodium/issues"
+ #define PACKAGE_URL "https://github.com/jedisct1/libsodium"
+ #define PACKAGE "libsodium"
+ #define VERSION "1.0.12"
+ #define MINIMAL 1
+ /* end confdefs.h.  */
+ 
+ int
+ main ()
+ {
+ 
+   ;
+   return 0;
+ }
